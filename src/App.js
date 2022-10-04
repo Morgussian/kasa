6e6a32cbda927e41cb285fec859a19614ad1a725
geraRD <simpsonwolfgang@mail.com>
2022-10-04 16:10:14 +0200

    affichage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css';
-import { Routes, Route} from 'react-router-dom';
+// import ReactDOM from 'react-dom/client';
+// import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Home from './screens/Home';
 import About from './screens/About';
 
 function App() {
   return (
+    
     <Routes>
+      
+            
       <Route path = '/' element = {<Home/>}/>
       <Route path = '/about' element = {<About/>}/>
       
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import '../src/components/styles/index.css';
 import App from './App';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
 
 
